b88eda1675bb6aeb8a4b0ae743239c5908b9b77c
MrColo <colmat967@gmail.com>
2024-05-23 15:47:28 +0200

    ff

--- src/storeProducts.s
+++ src/storeProducts.s
@@ -9,12 +9,23 @@
         .asciz "Ordini.txt" # Nome del file di testo da leggere
     fd:
         .int 0               # File descriptor
+    
+    id: .int 0
+    durata: .int 0
+    scadenza: .int 0
+    priorita: .int 0
 
     buffer: .space 256          # Buffer per la lettura del file
     buffer_width: .int 256      # Dimensione del buffer
     newline: .byte 10        # Valore del simbolo di nuova linea
     bytes_read: .int 0            # Numero di byte letti
     buffer_index: .int 0        # Indice per scorrere il buffer
+
+    comma:
+    .byte 44             # Valore del simbolo di virgola
+
+    malloc_size:
+    .int 40              # 10 prodotti x 4 byte ciascuno (32 bit per prodotto)
 
     read_error:
         .asciz "Errore nella apertura del file\n"
@@ -28,7 +39,29 @@
                         # il numero da convertire deve esse
 
 
+_move_ahead:
+    # Controlla se abbiamo raggiunto la fine dei dati letti nel buffer
+    mov bytes_read, %ecx
+    cmp %ecx, %edi
+    jge _read_file   # Se sì, torna a leggere dal file
+
+    # Carica il byte corrente del buffer in AL
+    mov buffer(,%edi,1), %al
+
+    # Incrementa l'indice del buffer
+    inc %edi
+
+    ret
+
 storeProducts:
+    # Passo 3: Allocazione della memoria per i prodotti
+    mov %eax, num_products
+    imul $4, %eax                 # 4 byte per prodotto
+    mov %eax, malloc_size
+    mov malloc_size, %ebx
+    mov $45, %eax                 # syscall brk
+    int $0x80
+    mov %eax, products_pointer    # Salva il puntatore alla memoria allocata
 
 # Apre il file
 _file_open:
@@ -44,7 +77,7 @@ _file_open:
     mov %eax, fd      # Salva il file descriptor da %eax a fd
 
 # Legge il file riga per riga
-_find_num_products:
+_read_file:
     mov $3, %eax        # syscall read
     mov fd, %ebx        # File descriptor
     mov $buffer, %ecx   # Buffer di input
@@ -58,27 +91,24 @@ _find_num_products:
     jle _file_close     # Se ci sono errori o EOF, chiudo il file
 
     # Resetta l'indice del buffer
-    movl $0, buffer_index
+    xor %edi, %edi
 
-_process_buffer:
-    # Controlla se abbiamo raggiunto la fine dei dati letti nel buffer
-    mov bytes_read, %ecx
-    cmp %ecx, buffer_index
-    jge _find_num_products   # Se sì, torna a leggere dal file
-
-    # Carica il byte corrente del buffer in AL
-    mov buffer_index, %ecx
-    mov buffer(,%ecx,1), %al
-
-    # Incrementa l'indice del buffer
-    incl buffer_index
-
+_find_ID:
+    call _move_ahead
     # Ad esempio, controlla se è una nuova linea o un delimitatore
-    cmp %al, newline
-    je _newline_found     # Gestisci la nuova linea
+    cmp %al, comma 
+    je _find_duration     # Gestisci la nuova linea
 
-    # Torna a processare il prossimo byte nel buffer
-    jmp _process_buffer
+
+
+    jmp _find_ID
+
+_find_duration:
+
+_find_deadline:
+
+_find_priority:
+
 
 _newline_found:
     incw num_products          # incremento il numero di prodotti
